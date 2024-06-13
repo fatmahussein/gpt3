e5a8c6b76ac977b3f46d59550fab0ad6366590fd
fatmahussein <fartunhussein346@gmail.com>
2024-06-13 23:16:13 +0300

    Create the blog section of the page

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imgUrl, date, title}) => {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-img'>
+        <img src={imgUrl} alt='blog'/>
+      </div>
+
+      <div className='gpt3__blog-container_article-content'>
+        <p>{date}</p>
+        <h3>{title}</h3>
+      </div>
+      <p className='read-article'>Read Full Article</p>
     </div>
   )
 }
